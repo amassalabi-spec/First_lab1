9c66d968edcd8676fa571c85570ed07cb121a874
MASSALABI OUMAROU Almajir <amassalabi@gmail.com>
2026-04-22 10:33:27 +0100

    Enhance web scraping with CSV output and pagination
    Refactored web scraping script to include CSV output and BeautifulSoup for parsing. Added functionality to extract repository details and handle pagination.

--- first_lab1.py.py
+++ first_lab1.py.py
@@ -1,5 +1,9 @@
 
-import requests
+import requests 
+from bs4 import BeautifulSoup
+import csv
+
+
 """commentaire
 before starting web scraping we create once an virtuale environnement for this project
 after we import the requests environment 
@@ -27,10 +31,35 @@ header = {
 
 page = requests.get(url, headers=header)
 
-print(page)
-s=page.text
-with open("test1.txt","w",encoding="utf-8") as f:
-    f.write(s)
 
-import os
-print("Le fichier est ici :", os.getcwd())
+#open a csv file to write the data we will extract from the website in it
+with open("test1.csv","w",encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["name of the repository", "description of the repository", "number of stars of the repository", "name of the owner of the repository", "date of the last update of the repository"])
+
+
+# i want to extract the name of the repository, the description of the repository, the number of stars of the repository, the name of the owner of the repository and the date of the last update of the repository
+# and put it in a csv file with the name of the repository, the description of the repository, the number of stars of the repository, the name of the owner of the repository and the date of the last update of the repository as header of the csv file
+
+# name of the classe of each post of the repersitory is "result-module_result_Up5vk" 
+while True:
+    soup = BeautifulSoup(s, "html.parser") 
+    repositories = soup.find_all('results-list', className='List-module__List__fNMbL')
+    for repository in repositories:
+        name = repository.find("div", class_="Result-module__Result__Up5vk").text.strip() 
+        name = repository.find("\n", "Header-module__header__sJ0g1").text.strip()
+        description = repository.find("p", class_="mb-1").text.strip() 
+        stars = repository.find("a", class_="Link--muted").text.strip()
+        owner = repository.find("a", class_="mr-1").text.strip()
+        date = repository.find("relative-time")["datetime"]
+        last_update = repository.find("relative-time").text.strip()
+        with open("test1.csv","a",encoding="utf-8") as f:
+            writer = csv.writer(f)
+            writer.writerow([name, description, stars, owner, date])
+    next_page = soup.find("a", class_="next_page")
+    if next_page:
+        url = "https://github.com/search?q=mental+health+ai&type=repositories" + next_page["href"]
+        page = requests.get(url, headers=header)
+        s=page.text
+    else:
+        break
